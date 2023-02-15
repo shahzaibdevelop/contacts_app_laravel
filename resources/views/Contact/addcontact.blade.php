46348586ef9acbacec49aa0e9f345f3d17f47dcc
Shahzaib Hassan <shahzaibdevelop@gmail.com>
2023-02-15 12:04:42 -0100

    Ui Update using bootstrap

--- resources/views/Contact/addcontact.blade.php
+++ resources/views/Contact/addcontact.blade.php
@@ -5,26 +5,50 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
    
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Add a contact</title>
+    <style>
+        form{
+            margin: 80px;
+        }
+        h1{
+            margin:50px 0px; 
+        }
+    </style>
 </head>
 <body>
-    <h1>Add a Contact</h1>
+    <h1 class="position-absolute top-0 start-50 translate-middle">Add a Contact</h1>
     <form action="submitcontact" method="POST">
         @csrf
-        Enter Name : <input type="text" name="name" >
+      
+        <div class="row mb-3">
+            <label for="name" class="col-sm-2 col-form-label">Name</label>
+            <div class="col-sm-10">
+              <input type="text" class="form-control" id="name" name="name">
+            </div>
+          </div>
         @error('name')            
         <br>
         <span style="color: red">{{ $message }}</span>
         @enderror
         <br>
-        Enter Number : <input type="text" name="contact" >
+        <div class="row mb-3">
+            <label for="contact" class="col-sm-2 col-form-label">Contact</label>
+            <div class="col-sm-10">
+              <input type="text" class="form-control" id="contact" name="contact">
+            </div>
+          </div>
         @error('contact')            
         <br>
         <span style="color: red">{{ $message }}</span>
         @enderror
         <br>
-        <button type="submit">Add Contact</button>
+        <button type="submit" class="btn btn-dark position-absolute top-50 start-50 translate-middle">Add Contact</button>
+
+         
     </form>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+   
 </body>
 </html>
